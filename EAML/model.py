78d1481215b4e37b18acaafc4af7896776c62f87
Kien Nguyen <kien.x.ng@gmail.com>
2022-04-28 18:23:02 -0400

    ayo lmao

--- EAML/model.py
+++ EAML/model.py
@@ -12,25 +12,25 @@ class Learner(nn.Module):
         self.vars_bn = nn.ParameterList()
 
         for i, (name, param) in enumerate(self.config):
-            if name is 'conv2d':
+            if name == 'conv2d':
                 w = nn.Parameter(torch.ones(*param[:4]))
                 torch.nn.init.kaiming_normal_(w)
                 self.vars.append(w)
                 self.vars.append(nn.Parameter(torch.zeros(param[0])))
 
-            elif name is 'convt2d':
+            elif name == 'convt2d':
                 w = nn.Parameter(torch.ones(*param[:4]))
                 torch.nn.init.kaiming_normal_(w)
                 self.vars.append(w)
                 self.vars.append(nn.Parameter(torch.zeros(param[1])))
 
-            elif name is 'linear':
+            elif name == 'linear':
                 w = nn.Parameter(torch.ones(*param))
                 torch.nn.init.kaiming_normal_(w)
                 self.vars.append(w)
                 self.vars.append(nn.Parameter(torch.zeros(param[0])))
 
-            elif name is 'bn':
+            elif name == 'bn':
                 w = nn.Parameter(torch.ones(param[0]))
                 self.vars.append(w)
                 self.vars.append(nn.Parameter(torch.zeros(param[0])))
@@ -50,26 +50,26 @@ class Learner(nn.Module):
         info = ''
 
         for name, param in self.config:
-            if name is 'conv2d':
+            if name == 'conv2d':
                 tmp = 'conv2d:(ch_in:%d, ch_out:%d, k:%dx%d, stride:%d, padding:%d)'%(param[1], param[0], param[2], param[3], param[4], param[5],)
                 info += tmp + '\n'
 
-            elif name is 'convt2d':
+            elif name == 'convt2d':
                 tmp = 'convTranspose2d:(ch_in:%d, ch_out:%d, k:%dx%d, stride:%d, padding:%d)'%(param[0], param[1], param[2], param[3], param[4], param[5],)
                 info += tmp + '\n'
 
-            elif name is 'linear':
+            elif name == 'linear':
                 tmp = 'linear:(in:%d, out:%d)'%(param[1], param[0])
                 info += tmp + '\n'
 
-            elif name is 'leakyrelu':
+            elif name == 'leakyrelu':
                 tmp = 'leakyrelu:(slope:%f)'%(param[0])
                 info += tmp + '\n'
 
-            elif name is 'avg_pool2d':
+            elif name == 'avg_pool2d':
                 tmp = 'avg_pool2d:(k:%d, stride:%d, padding:%d)'%(param[0], param[1], param[2])
                 info += tmp + '\n'
-            elif name is 'max_pool2d':
+            elif name == 'max_pool2d':
                 tmp = 'max_pool2d:(k:%d, stride:%d, padding:%d)'%(param[0], param[1], param[2])
                 info += tmp + '\n'
             elif name in ['flatten', 'tanh', 'relu', 'upsample', 'reshape', 'sigmoid', 'use_logits', 'bn']:
@@ -88,42 +88,42 @@ class Learner(nn.Module):
         bn_idx = 0
 
         for name, param in self.config:
-            if name is 'conv2d':
+            if name == 'conv2d':
                 w, b = vars[idx], vars[idx + 1]
                 x = F.conv2d(x, w, b, stride=param[4], padding=param[5])
                 idx += 2
-            elif name is 'convt2d':
+            elif name == 'convt2d':
                 w, b = vars[idx], vars[idx + 1]
                 x = F.conv_transpose2d(x, w, b, stride=param[4], padding=param[5])
                 idx += 2
-            elif name is 'linear':
+            elif name == 'linear':
                 w, b = vars[idx], vars[idx + 1]
                 x = F.linear(x, w, b)
                 idx += 2
-            elif name is 'bn':
+            elif name == 'bn':
                 w, b = vars[idx], vars[idx + 1]
                 running_mean, running_var = self.vars_bn[bn_idx], self.vars_bn[bn_idx+1]
                 x = F.batch_norm(x, running_mean, running_var, weight=w, bias=b, training=bn_training)
                 idx += 2
                 bn_idx += 2
 
-            elif name is 'flatten':
+            elif name == 'flatten':
                 x = x.view(x.size(0), -1)
-            elif name is 'reshape':
+            elif name == 'reshape':
                 x = x.view(x.size(0), *param)
-            elif name is 'relu':
+            elif name == 'relu':
                 x = F.relu(x, inplace=param[0])
-            elif name is 'leakyrelu':
+            elif name == 'leakyrelu':
                 x = F.leaky_relu(x, negative_slope=param[0], inplace=param[1])
-            elif name is 'tanh':
+            elif name == 'tanh':
                 x = F.tanh(x)
-            elif name is 'sigmoid':
+            elif name == 'sigmoid':
                 x = torch.sigmoid(x)
-            elif name is 'upsample':
+            elif name == 'upsample':
                 x = F.upsample_nearest(x, scale_factor=param[0])
-            elif name is 'max_pool2d':
+            elif name == 'max_pool2d':
                 x = F.max_pool2d(x, param[0], param[1], param[2])
-            elif name is 'avg_pool2d':
+            elif name == 'avg_pool2d':
                 x = F.avg_pool2d(x, param[0], param[1], param[2])
 
             else:
